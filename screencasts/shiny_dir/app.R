ac6c3b64f137551f9f1e2ad0c6b2e4d1225ff66c
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2026-01-04 21:10:54 +0100

    shine bright likea a ...

---
+++ screencasts/shiny_dir/app.R
@@ -0,0 +1,31 @@
+library(bslib)
+
+ui <- page_sidebar(
+    title = "Hello Shiny!",
+    sidebar = sidebar(
+        sliderInput(
+            inputId = "bins",
+            label = "Number of bins:",
+            min = 1,
+            max = 50,
+            value = 30
+        )
+    ),
+    # Output: Histogram ----
+    plotOutput(outputId = "distPlot")
+)
+
+server <- function(input, output) {
+    output$distPlot <- renderPlot({
+        x    <- faithful$waiting
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        
+        hist(x, breaks = bins, col = "#007bc2", border = "white",
+             xlab = "Waiting time to next eruption (in mins)",
+             main = "Histogram of waiting times")
+        
+    })
+    
+}
+
+shinyApp(ui = ui, server = server)
